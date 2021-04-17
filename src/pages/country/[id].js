14d621433ba8e363222adf600029b6e00ed07372
shoaib ahamed <shoaibahamedshafi@gmail.com>
2021-04-17 14:17:12 +0600

    fast reload & fav icon

--- src/pages/country/[id].js
+++ src/pages/country/[id].js
@@ -121,11 +121,26 @@ const Country = ({country}) => {
 
 export default Country;
 
-export const getServerSideProps = async  ({params}) => {
-    const country = await getCountry(params.id);
+export const getStaticPaths = async () => {
+    const res = await fetch("https://restcountries.eu/rest/v2/all");
+    const countries = await res.json();
+  
+    const paths = countries.map((country) => ({
+      params: { id: country.alpha3Code },
+    }));
+  
     return {
-        props: {
-            country
-        }
-    }
-}
+      paths,
+      fallback: false,
+    };
+  };
+  
+  export const getStaticProps = async ({ params }) => {
+    const country = await getCountry(params.id);
+  
+    return {
+      props: {
+        country,
+      },
+    };
+  };
